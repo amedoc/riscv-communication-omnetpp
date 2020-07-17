64455b825d390beb2a096aab182157a2cf0d7fc7
amedoc <ahmed.mahmoudi.mk@gmail.com>
2020-07-17 14:36:32 +0200

    working riscv simulation under example1 with riscv module under sc_main

--- example1/forkjoin.cc
+++ example1/forkjoin.cc
@@ -115,8 +115,81 @@ public:
 
   void main()
   {
+/*
+      Options opt ; //= parse_command_line_arguments(argc, argv);
 
+           std::srand(std::time(nullptr));  // use current time as seed for random generator
 
+           tlm::tlm_global_quantum::instance().set(sc_core::sc_time(opt.tlm_global_quantum, sc_core::SC_NS));
+
+           ISS core(0, opt.use_E_base_isa);
+           MMU mmu(core);
+           CombinedMemoryInterface core_mem_if("MemoryInterface0", core, &mmu);
+           SimpleMemory mem("SimpleMemory", opt.mem_size);
+           ELFLoader loader(opt.input_program.c_str());
+           SimpleBus<2, 3> bus("SimpleBus");
+           SyscallHandler sys("SyscallHandler");
+           CLINT<1> clint("CLINT");
+           DebugMemoryInterface dbg_if("DebugMemoryInterface");
+
+           MemoryDMI dmi = MemoryDMI::create_start_size_mapping(mem.data, opt.mem_start_addr, mem.size);
+           InstrMemoryProxy instr_mem(dmi, core);
+
+           std::shared_ptr<BusLock> bus_lock = std::make_shared<BusLock>();
+           core_mem_if.bus_lock = bus_lock;
+
+           instr_memory_if *instr_mem_if = &core_mem_if;
+           data_memory_if *data_mem_if = &core_mem_if;
+           if (opt.use_instr_dmi)
+               instr_mem_if = &instr_mem;
+           if (opt.use_data_dmi) {
+               core_mem_if.dmi_ranges.emplace_back(dmi);
+           }
+
+           loader.load_executable_image(mem.data, mem.size, opt.mem_start_addr);
+           core.init(instr_mem_if, data_mem_if, &clint, loader.get_entrypoint(), rv32_align_address(opt.mem_end_addr));
+           sys.init(mem.data, opt.mem_start_addr, loader.get_heap_addr());
+           sys.register_core(&core);
+
+           if (opt.intercept_syscalls)
+               core.sys = &sys;
+
+           // setup port mapping
+           bus.ports[0] = new PortMapping(opt.mem_start_addr, opt.mem_end_addr);
+           bus.ports[1] = new PortMapping(opt.clint_start_addr, opt.clint_end_addr);
+           bus.ports[2] = new PortMapping(opt.sys_start_addr, opt.sys_end_addr);
+
+           // connect TLM sockets
+           core_mem_if.isock.bind(bus.tsocks[0]);
+           dbg_if.isock.bind(bus.tsocks[1]);
+           bus.isocks[0].bind(mem.tsock);
+           bus.isocks[1].bind(clint.tsock);
+           bus.isocks[2].bind(sys.tsock);
+
+           // connect interrupt signals/communication
+           clint.target_harts[0] = &core;
+
+           // switch for printing instructions
+           core.trace = opt.trace_mode;
+
+           std::vector<debug_target_if *> threads;
+           threads.push_back(&core);
+
+           if (opt.use_debug_runner) {
+               auto server = new GDBServer("GDBServer", threads, &dbg_if, opt.debug_port);
+               new GDBServerRunner("GDBRunner", server, &core);
+           } else {
+               new DirectCoreRunner(core);
+           }
+
+           if (opt.quiet)
+                sc_core::sc_report_handler::set_verbosity_level(sc_core::SC_NONE);
+
+           //sc_core::sc_start();
+           if (!opt.quiet) {
+               core.show();
+           }
+*/
   }
 
 
@@ -126,7 +199,7 @@ int sc_main (int argc , char *argv[])
 {
   top top1("Top1");
 
-  /*Options opt ; //= parse_command_line_arguments(argc, argv);
+  Options opt ; //= parse_command_line_arguments(argc, argv);
 
       std::srand(std::time(nullptr));  // use current time as seed for random generator
 
@@ -198,7 +271,7 @@ int sc_main (int argc , char *argv[])
       //sc_core::sc_start();
       if (!opt.quiet) {
           core.show();
-      }*/
+      }
   sc_start();
 
   return 0;
